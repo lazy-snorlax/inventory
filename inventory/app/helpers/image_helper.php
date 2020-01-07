92896aa9482a277e56eaafca7e0be030a02a3c0e
Anthony Morelli <blacknebula236@gmail.com>
2020-01-08 03:11:04 +1030

    Added image functions
    Added a function that rewrites the 'image_location' from the database to the public/uploads folder (doesn't actually edit the database). Added a function that empties the public/uploads folder. This is used whenever an item is deleted (and the items/index refills it).

--- inventory/app/helpers/image_helper.php
+++ inventory/app/helpers/image_helper.php
@@ -1,6 +1,22 @@
 <?php
-  // Move image to public/uploads folder
-  function moveImage($imageName){
-    print_r($imageName);
-    rename($imageName, 'public/uploads/');
+  // Change the image_location from app/uploads to the public/uploads folder
+  function renameImageLocation($image_location){
+    $imageLocation = explode('\\', $image_location);
+    $imageNewName = 'public/'.$imageLocation[5].'/'.$imageLocation[6];
+
+    return $imageNewName;
+  }
+
+  // Empty public uploads folder
+  /** This comes accross as hardcoded. Test in a live environment to see what tweaks are needed */
+  function emptyPublicUploads(){
+    $filepath = explode('\\', APPROOT);
+    $files = glob($filepath[0].'//'.$filepath[1].'//'.$filepath[2].'//'.$filepath[3].'/public/uploads/*');
+
+    foreach ($files as $file){
+      if(is_file($file)){
+        unlink($file);
+      }
+    }
+
   }
